fbf63684ffdabe971fe32deff32e78b0eb533616
WorKai7 <jvcraft59@gmail.com>
2025-04-02 00:14:03 +0200

    feat(auth): Redesign de la page de login

--- wwe/inc/head.php
+++ wwe/inc/head.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/styles.css" />
-    <link rel="stylesheet" href="/wwe/css/uikit-3.23.4/css/uikit.min.css"/>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <script src="js/scripts.js"></script>
 </head>
 
--- wwe/login.php
+++ wwe/login.php
@@ -15,98 +15,78 @@ $db = require(dirname(__FILE__) . '/lib/mypdo.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/styles.css">
-    <script src="js/scripts.js"></script>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <style>
+        .fullscreen-bg {
+            background: url('img/login_background.jpeg') no-repeat center center;
+            background-size: cover;
+            min-height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="maincontent  w3-display-container w3-center">
-        <div class="dtitle w3-container w3-teal">
-            <h1>Bienvenue sur le portail de votre projet !</h1>
+    <div class="fullscreen-bg d-flex flex-column">
+        <div class="card p-3 mb-3">
+            <div class="card-body">
+                <h2 class="text-center mb-5">Connexion</h2>
+                <form class="d-flex flex-column align-items-center" action="check_login.php" method="post">
+                    <div class="mb-4">
+                        <input class="form-control" type="text" placeholder="Nom d'utilisateur" name="uname" required>
+                    </div>
+                    <div class="mb-2">
+                        <input class="form-control" type="password" placeholder="Mot de passe" name="psw" required>
+                    </div>
+                    <a class="mb-4">S'inscrire</a>
+                    <?php if (is_null($db)) {
+                    ?>
+                        <button class="btn btn-danger" type="submit" name="connect" disabled>Impossible de se connecter</button>
+                    <?php
+                    } else {
+                    ?>
+                        <button class="btn btn-primary" type="submit" name="connect">Se connecter</button>
+                    <?php
+                    }
+                    ?>
+                </form>
+            </div>
         </div>
 
-        <form action="check_login.php" method="post">
-            <div class="container">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Identifiant" name="uname" required>
+        <?php
+        if ($db) {
+            $db = NULL;
+        }
+        if (is_array($_SESSION['mesgs']) && is_array($_SESSION['mesgs']['confirm'])) {
+            foreach ($_SESSION['mesgs']['confirm'] as $mesg) {
+        ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <?= $mesg; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fermer"></button>
+                </div>
+            <?php
+            }
+            unset($_SESSION['mesgs']['confirm']);
+        }
+        if (is_array($_SESSION['mesgs']) && is_array($_SESSION['mesgs']['errors'])) {
+            foreach ($_SESSION['mesgs']['errors'] as $err) {
+            ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?= $err; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fermer"></button>
+                </div>
+        <?php
+            }
+            unset($_SESSION['mesgs']['errors']);
+        }
+        ?>
 
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Mot de passe" name="psw" required>
-                <?php if (is_null($db)) {
-                ?>
-                    <input type="submit" name="connect" value="Impossible de Se connecter" disabled />
-                <?php
-                } else {
-                ?>
-                    <input type="submit" name="connect" value="Se connecter" />
-                <?php
-                }
-                ?>
-            </div>
-        </form>
     </div>
     <div style="clear:both;"></div>
-    <?php
-    if ($db) {
-        $db = NULL;
-    }
-    if (is_array($_SESSION['mesgs']) && is_array($_SESSION['mesgs']['confirm'])) {
-        foreach ($_SESSION['mesgs']['confirm'] as $mesg) {
-    ?>
-            <div class="alertbox messagebox">
-                <span class="closebtn">&times;</span>
-                <?= $mesg; ?>
-            </div>
-        <?php
-        }
-        unset($_SESSION['mesgs']['confirm']);
-    }
-    if (is_array($_SESSION['mesgs']) && is_array($_SESSION['mesgs']['errors'])) {
-        foreach ($_SESSION['mesgs']['errors'] as $err) {
-        ?>
-            <div class="alertbox errorbox">
-                <span class="closebtn">&times;</span>
-                <?= $err; ?>
-            </div>
-    <?php
-        }
-        unset($_SESSION['mesgs']['errors']);
-    }
-    ?>
-
-    <script>
-        // Get all elements with class="closebtn"
-        var close = document.getElementsByClassName("closebtn");
-        var i;
-
-        // Loop through all close buttons
-        for (i = 0; i < close.length; i++) {
-            // When someone clicks on a close button
-            close[i].onclick = function() {
-
-                // Get the parent of <span class="closebtn"> (<div class="alert">)
-                var div = this.parentElement;
-
-                // Set the opacity of div to 0 (transparent)
-                div.style.opacity = "0";
-
-                // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
-                setTimeout(function() {
-                    div.style.display = "none";
-                }, 600);
-            }
-        }
-    </script>
-    <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
-        <i class="fab fa-facebook w3-hover-opacity" aria-hidden="true"></i>
-        <i class="fab fa-instagram w3-hover-opacity" aria-hidden="true"></i>
-        <i class="fab fa-snapchat w3-hover-opacity" aria-hidden="true"></i>
-        <i class="fab fa-pinterest-p w3-hover-opacity" aria-hidden="true"></i>
-        <i class="fab fa-twitter w3-hover-opacity" aria-hidden="true"></i>
-        <i class="fab fa-linkedin w3-hover-opacity" aria-hidden="true"></i>
-        <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-    </footer>
-
 </body>
 
 </html>
